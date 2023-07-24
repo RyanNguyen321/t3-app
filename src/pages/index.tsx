6588cfd3dc3fafe646ae37b50a1568ca03a9290e
RyanNguyen321 <nguyenryan0903@gmail.com>
2023-07-24 15:45:21 -0400

    third commit, added auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,10 @@ import React from "react";
 import {SignIn, SignInButton, SignOutButton, useUser} from "@clerk/nextjs";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
-  const user = useUser();
+    const user = useUser();
+    const {data} = api.posts.getAll.useQuery();
+
 
   return (
     <>
@@ -21,7 +22,12 @@ export default function Home() {
             {!user.isSignedIn && <SignInButton/>}
             {user.isSignedIn && <SignOutButton/>}
         </div>
-      <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up"/>
+      <div>
+          {data?.map((post) => (
+              <div key={post.id}> {post.content} </div>
+          ))}
+      </div>
+
       </main>
     </>
   );
